fca4ccababce2f10b7f782d031c1cf996c21b8c3
Jyotsana <jyotsana0936@gmail.com>
2026-03-26 03:48:42 +0530

    uc7

---
+++ uc7.java
@@ -0,0 +1,58 @@
+public class Hello {
+
+    static class CharacterPattern {
+        char character;
+        String[] pattern;
+
+        CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, CharacterPattern> map = new HashMap<>();
+
+        map.put('O', new CharacterPattern('O', new String[]{
+                " ****** ",
+                "**    **",
+                "**    **",
+                "**    **",
+                " ****** "
+        }));
+
+        map.put('P', new CharacterPattern('P', new String[]{
+                " ****** ",
+                "**    **",
+                " ****** ",
+                "**      ",
+                "**      "
+        }));
+
+        map.put('S', new CharacterPattern('S', new String[]{
+                " ****** ",
+                "**      ",
+                " ****** ",
+                "      **",
+                " ****** "
+        }));
+
+        String word = "OOPS";
+
+        for (int i = 0; i < 5; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (int j = 0; j < word.length(); j++) {
+                CharacterPattern cp = map.get(word.charAt(j));
+                line.append(cp.getPattern()[i]).append("  ");
+            }
+
+            System.out.println(line);
+        }
+    }
+}
