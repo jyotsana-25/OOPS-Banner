a1151399bfd3ffe01d827181506cd0e0455b12e5
jyotsana-25 <jyotsana0936@gmail.com>
2026-03-26 02:03:20 +0530

    Add files via upload

---
+++ Hello3.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        
+        System.out.println(String.join(" ", "  *** ", "  *** ", " **** ", " ***** "));
+        
+        System.out.println(String.join(" ", " ** **", " ** **", " ** **", "**  "));
+        
+        System.out.println(String.join(" ", " ** **", " ** **", " **** ", "**** "));
+        
+        System.out.println(String.join(" ", " ** **", " ** **", " ** ", "     **"));
+        
+        System.out.println(String.join(" ", " ** **", " ** **", " ** ", "     **"));
+        
+        System.out.println(String.join(" ", "  *** ", "  *** ", " ** ", " ***** "));
+        
+    }
+}
