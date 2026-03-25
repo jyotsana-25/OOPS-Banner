8b3ff9357a410aa25128eb667152220d9430a99e
jyotsana-25 <jyotsana0936@gmail.com>
2026-03-26 02:00:08 +0530

    Add files via upload

---
+++ uc5.java
@@ -0,0 +1,19 @@
+public class PrintOOPS {
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "  *****      *****      *****      *****    ",
+            " **    **   **    **    **   **   **        ",
+            " **    **   **    **    **   **   **        ",
+            " **    **   **    **    ******      *****   ",
+            " **    **   **    **    **               ** ",
+            " **    **   **    **    **         **    ** ",
+            "  *****      *****      **           *****  ",
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
+    
